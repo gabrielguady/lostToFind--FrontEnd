f470b10a5d4f3a071014c8e28938a867d5efa37a
speedwwr <gui.maciel.correa20042gmail.com>
2025-06-16 20:37:01 -0400

    last updates

--- front-end-lostToFind/src/base/found-list/found-item-details/found-item-details.component.ts
+++ front-end-lostToFind/src/base/found-list/found-item-details/found-item-details.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import {CommentsComponent} from "../../comments/comments.component";
+import {DatePipe, DecimalPipe} from "@angular/common";
+import {MatCard, MatCardContent} from "@angular/material/card";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-found-item-details',
   standalone: true,
-  imports: [],
+    imports: [
+        CommentsComponent,
+        DatePipe,
+        DecimalPipe,
+        MatCard,
+        MatCardContent,
+        MatIcon,
+        MatIconButton
+    ],
   templateUrl: './found-item-details.component.html',
   styleUrl: './found-item-details.component.scss'
 })
